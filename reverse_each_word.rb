6335fac026f8f9f52c6b76527b7755006cdf7b83
Henry Phan <h.phan06@gmail.com>
2019-11-18 22:52:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,17 @@
 require 'pry'
 
 
-def reverse_each_word(sentence)
-  sentence.split.collect {|r| r.reverse}.join(" ")
-  # sentence.split(" ").reverse.join(" ").reverse
+# def reverse_each_word(sentence)
+#   sentence.split.collect {|r| r.reverse}.join(" ")
+#   # sentence.split(" ").reverse.join(" ").reverse
   
 
-  #binding.pry
+#   #binding.pry
+# end
+
+
+def reverse_each_word(string)
+  split_string = string.split(" ") #=> splits string into seperate parts
+  string.split.each()
+  binding.pry
 end
