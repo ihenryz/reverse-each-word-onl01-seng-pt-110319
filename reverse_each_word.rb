c5a4fa4433474c17e6891cfaa283fb48935aab6b
Henry Phan <h.phan06@gmail.com>
2019-11-19 00:14:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,10 @@
 require 'pry'
 
 
-# def reverse_each_word(sentence)
-#   sentence.split.collect {|r| r.reverse}.join(" ")
-#   # sentence.split(" ").reverse.join(" ").reverse
+def reverse_each_word(sentence)
+  sentence.split.collect {|r| r.reverse}.join(" ")
+  # sentence.split(" ").reverse.join(" ").reverse
   
 
-#   #binding.pry
-# end
-
-
-def reverse_each_word(string)
-  string.split(" ")
+  #binding.pry
 end
